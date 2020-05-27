d1d762e45d1ccaec4eb5201f0b7bf5c7b9cb828a
Prasun Paul Chowdhury <prasun@novatree.com>
2020-05-27 21:07:24 +0530

    Adds space between two line

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -73,6 +73,7 @@
                             <button type="submit" class="btn btn-outline-primary">
                                 {{ __('Login') }}
                             </button>
+
                             @if (Route::has('password.request'))
                                 <a class="btn btn-link" href="{{ route('password.request') }}">
                                     {{ __('Forgot Your Password?') }}
