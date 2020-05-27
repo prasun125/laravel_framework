5e8643cd9ed364c4b10313e98ae2df10e86ec86d
Prasun Paul Chowdhury <prasun@novatree.com>
2020-05-27 20:24:11 +0530

    Changes home blade

--- resources/views/pages/home.blade.php
+++ resources/views/pages/home.blade.php
@@ -1,7 +1,9 @@
-@extends('layouts.app')
-
+@extends('layouts.master')
+@section('title')
+    Dashboard |
+@endsection
 @section('content')
-<div class="container">
+<div class="container p-t-6rem">
     <div class="row justify-content-center">
         <div class="col-md-8">
             <div class="card">
