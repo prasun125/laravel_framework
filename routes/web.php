19c737090bd2db615bac52210246c6e0b06a1e37
Prasun Paul Chowdhury <prasun@novatree.com>
2020-05-31 20:57:34 +0530

    Creates test route

--- routes/web.php
+++ routes/web.php
@@ -21,3 +21,7 @@ Route::get('/', function () {
 Auth::routes();
 
 Route::get('/home', 'HomeController@index')->name('home');
+
+Route::get('/test', function () {
+    return view('pages.test');
+});
